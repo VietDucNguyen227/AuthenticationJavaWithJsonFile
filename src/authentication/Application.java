ee02ba967a45b907fee701075e4cf066091ef08d
VietDucNguyen227 <shiroukrs1>
2022-03-25 17:08:39 +0700

    first commit

---
+++ src/authentication/Application.java
@@ -0,0 +1,8 @@
+package authentication;
+
+public class Application {
+    public static void main(String[] args) throws Exception {
+        Controller controller = new Controller();
+        controller.home();
+    }
+}
